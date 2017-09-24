69f807a23ef6319d32466ed49eb30bbba357d007
Tham Shi Yuan <thamsy94@gmail.com>
2017-09-24 13:58:45 -0700

    Add demo comments

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ def get_bike_recommendations():
 
     return jsonify(all_bikes["results"][:3])
 
+# For Demo
 @app.route("/v1/get_bike_recommendations_constants", methods=['GET'])
 def get_bike_recommendations_constants():
     return jsonify(constants.bikes)
